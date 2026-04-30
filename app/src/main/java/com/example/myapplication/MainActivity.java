6907c377a6b93177a08b7cbc7fb9f554adb80255
𝐹𝑜𝑥 <foxsd520@gmail.com>
2026-04-30 17:14:02 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,11 @@
-
-package com.foxsd.browsercom.example.myapplication;
+package com.foxsd.browser;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import com.example.myapplication.databinding.ActivityMainBinding;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import com.foxsd.browser.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -12,13 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Inflate and get instance of binding
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-
-        // set content view to binding's root
         setContentView(binding.getRoot());
+
+        WebView webView = binding.webview;
+        WebSettings ws = webView.getSettings();
+        ws.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient()); // يبقى داخل التطبيق
+        webView.loadUrl("https://www.google.com"); // غيّر الرابط إذا أردت
     }
-    
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
